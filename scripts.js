83e4dba313e9ff5c51ce685eeab2b68da94d5e7e
Isaiah Chong Kai Ean <isaiahchong@gtm.net>
2023-12-18 17:54:45 +0800

    added carousel into resume

--- scripts.js
+++ scripts.js
@@ -16,16 +16,25 @@
     let currentMode= document.querySelector('html');
     let darkModeToggle= document.querySelector('#darkMode');
     let darkLightIcon= document.querySelector('#modeIcon');
+    let bgImage= document.querySelector('#bgImage');
+    let myName= document.querySelector('#myName');
     darkModeToggle.addEventListener('click', function(evt){
         evt.preventDefault();
         console.log (currentMode.getAttribute('data-bs-theme'));
         if (currentMode.getAttribute('data-bs-theme') == "light"){
             currentMode.setAttribute('data-bs-theme','dark');
             darkLightIcon.setAttribute('src','images/darkMode.png')
+            bgImage.setAttribute('src','images/sceneryDark.jpg')
+            myName.removeAttribute('class')
+            myName.setAttribute('class','text-light')
+            
         }
         else if (currentMode.getAttribute('data-bs-theme') == "dark"){
             currentMode.setAttribute('data-bs-theme','light');
             darkLightIcon.setAttribute('src','images/lightMode.png')
+            bgImage.setAttribute('src','images/sceneryLight.jpg')
+            myName.removeAttribute('class')
+            myName.setAttribute('class','text-dark')
         }
         else{
             console.log('Error toggle dark-light mode')
